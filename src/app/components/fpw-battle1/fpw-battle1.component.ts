ff5cd97fa85286f89e40ad12f28b4c1c30598b22
Alfonso <alfonso.martinez.cabanillas@gmail.com>
2021-01-10 18:32:20 +0100

    Implement turn function

--- src/app/components/fpw-battle1/fpw-battle1.component.ts
+++ src/app/components/fpw-battle1/fpw-battle1.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { Ship } from '../../models/ship';
-import { AnimationBuilder, NoopAnimationPlayer, AnimationPlayer, stagger, trigger, transition, style, state, query, group, animate, keyframes } from '@angular/animations';
+import { AnimationBuilder, NoopAnimationPlayer, AnimationPlayer, stagger, trigger, transition, style, state, query, group, sequence, animate, keyframes } from '@angular/animations';
 import { HAMMER_LOADER } from '@angular/platform-browser';
 import { bindCallback } from 'rxjs';
 
@@ -30,10 +30,10 @@ export class FpwBattle1Component implements OnInit {
 
   
   /* Carthaginian squadrons I to IV*/
-  public squadronC1: Ship[] =  this.buildSquadron({id:"squadronC1", x0: 25, y0: 71, xn:30, yn: 85, size: 10 });
+  public squadronC1: Ship[] =  this.buildSquadron({id:"squadronC1", x0: 25, y0: 75, xn:30, yn: 85, size: 8 });
   public squadronC2: Ship[] = this.buildSquadron({id:"squadronC2", x0: 25, y0: 51, xn: 25, yn: 70, size: 10 });
   public squadronC3: Ship[] = this.buildSquadron({id:"squadronC3", x0: 25, y0: 49, xn: 25, yn: 30, size: 10 });
-  public squadronC4: Ship[] = this.buildSquadron({id:"squadronC4", x0: 25, y0: 29, xn: 30, yn: 15, size: 10 });
+  public squadronC4: Ship[] = this.buildSquadron({id:"squadronC4", x0: 25, y0: 25, xn: 30, yn: 15, size: 8 });
 
   public player: AnimationPlayer = new NoopAnimationPlayer();
 
@@ -72,12 +72,10 @@ export class FpwBattle1Component implements OnInit {
 
   private _moveShipAnimation( shipQuery: string, position: string[], orientation:number, time:number, delay:number, stagger: number){
     var total_delay=delay+stagger; 
-    
-    
       var rotation = 'rotate('+orientation+'deg)';
       return this._builder.build([
         query(shipQuery, [ group([
-          animate( time/4+'s', style({transform: rotation})),
+          animate( time/2+'s', style({transform: rotation})),
           animate(time+'s '+total_delay+'s ease-in', style({left: position[0], bottom: position[1]}))
         ])
         ])
@@ -110,13 +108,15 @@ export class FpwBattle1Component implements OnInit {
       var x = x0 + i * xSpace;
       var y = y0 + i * ySpace;
      
+       //var orientation = Math.atan2((y-old_squadron[i].y), (x-old_squadron[i].x))*180/Math.PI;
        if (x-old_squadron[i].x>0){
-        var orientation = Math.atan((y-old_squadron[i].y)/(x-old_squadron[i].x))*180/3.14*(-1);
+        var orientation = Math.atan((y-old_squadron[i].y)/(x-old_squadron[i].x))*180/Math.PI*(-1);
       } else if(x-old_squadron[i].x<0) { 
-        var orientation = Math.atan((y-old_squadron[i].y)/(x-old_squadron[i].x))*180/3.14;
+        var orientation = Math.atan((y-old_squadron[i].y)/(x-old_squadron[i].x))*180/Math.PI;
       } else {
-        var orientation=3.14/2;
+        var orientation=Math.PI/2;
       }
+      
       squadron[i].x=x;
       squadron[i].y=y;
       squadron[i].orientation=orientation; 
@@ -134,19 +134,40 @@ export class FpwBattle1Component implements OnInit {
     }
   }
 
-  private _turnSquadronAnimation( shipQuery: string, time:number, rotation:string){ 
-      return this._builder.build([
-        query('.ship', [ group([
-          animate( time +'s', style({transformOrigin: '100% 100%' , transform: 'rotate(180deg)'})),
-        ])
-        ])
-      ])
+  private _turnSquadronAnimation(squadron, orientation:number, origin:string, arc:number, time:number){ 
+    var squadronId=squadron[0].id;
+    var imgQuery='#'+squadronId+' .imgShip';
+    let shipQuery='#'+squadronId+'> .ship';
+    var rotation = 'rotate('+orientation+'deg)';
+    var arcRotation = 'rotate('+arc+'deg)';
+
+    return this._builder.build(sequence([
+        query(imgQuery, [ 
+         animate('2s', style({transformOrigin: '50% 50%' , transform: rotation  }))
+          ]),
+        group([
+          query(shipQuery, [
+          animate( time +'s', style({transformOrigin: origin , transform: arcRotation }))
+          ]),
+        ]),  
+      ])   
+      )    
   }
 
-  private _turnSquadronPlayer(shipQuery:string, time:number, rotation:string) {
-    const animation = this._turnSquadronAnimation(shipQuery, time, rotation);
+  // 'rotate(90deg)'  '800% 0%' 'rotate(-160deg)'
+
+  private _turnSquadronPlayer(squadron, orientation:number, origin:string, arc:number, time:number) {
+    const animation = this._turnSquadronAnimation(squadron, orientation, origin, arc, time);
     return animation.create(this._element.nativeElement);
   }
+
+  private _turnSquadron(squadron, orientation:number, origin:string, arc:number, time:number, callback=void(0)) {
+    var player=this._turnSquadronPlayer(squadron, orientation, origin, arc, time);
+    player.play();
+
+  }
+
+
   
   
 
@@ -157,7 +178,6 @@ export class FpwBattle1Component implements OnInit {
       this.stage = "stage" + this.stageCounter;
       switch (this.stageCounter) {
         case 1: {
-          //this._turnSquadronPlayer("", 5, "").play();
           this._moveSquadron(this.squadronR1, { x0: 30, y0: 50.5, xn: 40, yn:65},20,0,0);
           this._moveSquadron(this.squadronR2, { x0: 30, y0: 49.5, xn: 40, yn:35},20,0,0);
           this._moveSquadron(this.squadronR3, { x0: 60, y0: 40, xn: 60, yn: 60},18,2,0);
@@ -175,24 +195,20 @@ export class FpwBattle1Component implements OnInit {
           this._moveSquadron(this.squadronR4, { x0: 55, y0: 35, xn: 55, yn: 65},5,0,0);
           this._moveSquadron(this.squadronC2, { x0: 24, y0: 50.5, xn: 26, yn:67},5,0,0.2);
           this._moveSquadron(this.squadronC3, { x0: 24, y0: 49.5, xn: 26, yn:33},5,0,0.1);
-          this._moveSquadron(this.squadronC4, { x0: 35, y0: 25, xn: 35, yn: 10},2,0,0, ()=>{
-            this._moveSquadron(this.squadronC4, { x0: 50, y0: 25, xn: 50, yn: 10},2,0, 0, ()=>{
-              this._moveSquadron(this.squadronC4, { x0: 55, y0: 30, xn: 60, yn: 20},2,0, 0)
-            })
-          });
+        //  this._turnSquadronPlayer(this.squadronC4, 40, '400% -600%', -80, 5).play();
           break
         }
 
         case 3: {
-          this._moveSquadron(this.squadronR3, { x0: 50, y0: 40, xn: 50, yn: 60},5,0, 0);
+         /* this._moveSquadron(this.squadronR3, { x0: 50, y0: 40, xn: 50, yn: 60},5,0, 0);
           this._moveSquadron(this.squadronR4, { x0: 55, y0: 37, xn: 65, yn: 55},5,0, 0.1);
-          this._moveSquadron(this.squadronC4, { x0: 56, y0: 34, xn: 70, yn: 33},5,0, 0.1);
+          this._moveSquadron(this.squadronC4, { x0: 56, y0: 34, xn: 70, yn: 33},5,0, 0.1);*/
           break
         }
 
         case 4: {
-          this._moveSquadron(this.squadronR4, { x0: 56, y0: 35, xn: 68, yn: 39},5,0, 0.1);
-          this._moveSquadron(this.squadronC4, { x0: 56, y0: 34, xn: 69, yn: 38},5,0, 0.1);
+         /* this._moveSquadron(this.squadronR4, { x0: 56, y0: 35, xn: 68, yn: 39},5,0, 0.1);
+          this._moveSquadron(this.squadronC4, { x0: 56, y0: 34, xn: 69, yn: 38},5,0, 0.1);*/
           break
         }
 
